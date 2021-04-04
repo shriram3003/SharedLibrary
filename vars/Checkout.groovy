9c4a03046a6e23660f5de1a5097d52642b7a7b99
shriram3003 <81859136+shriram3003@users.noreply.github.com>
2021-04-04 20:15:50 +0530

    Update Checkout.groovy

--- vars/Checkout.groovy
+++ vars/Checkout.groovy
@@ -1,2 +1,7 @@
-def call()
-{}
+def call() {  
+  checkout([$class: 'GitSCM', 
+      branches: [[name: '*/main']], 
+      extensions: [], 
+      userRemoteConfigs: [[credentialsId: 'b301cd92-1cd3-4e71-aeba-2864fcf1d58e', 
+      url: 'https://github.com/shriram3003/Release_Report.git']]])  
+  }
